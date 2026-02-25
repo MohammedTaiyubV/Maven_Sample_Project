f868474c72757e36544c8ff671a145b60312103b
KabshaTaiyub <kabshataiyub@gmail.com>
2026-02-25 09:29:57 +0530

    commit

--- src/main/java/com/louder/App.java
+++ src/main/java/com/louder/App.java
@@ -1,13 +1,21 @@
 package com.louder;
-
-/**
+import java.util.Scanner;
+/** 
  * Hello world!
  *
  */
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {  
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.println( "Enter 2 numbers" );
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // int result = a+b;
+
+        
+        System.out.println("Result: " +(a+b));
     }
 }

35d5c19a4e40389c7cfbfe1f6c7f24bbd00007fe
Mohammed Taiyub V <144512125+MohammedTaiyubV@users.noreply.github.com>
2026-04-18 07:23:07 +0530

    Update App.java

--- src/main/java/com/louder/App.java
+++ src/main/java/com/louder/App.java
@@ -3,7 +3,7 @@ package com.louder;
 class Fact {
     public static void main(String[] args) {
 
-        int n = 6, fact = 1, temp = n;
+        int n = 9, fact = 1, temp = n;
 
         while (n > 1) {
             fact *= n--;
